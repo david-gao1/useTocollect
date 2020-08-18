d4d70f13f447dabbe1cebb54f0e924ba45a8b2b4
v_liangggao <v_liangggao@email.com>
2020-08-18 12:18:08 +0800

    0002、map的使用小结一：遍历，contain

--- uselang3/src/main/java/com/practise/useEnum/useEnums.java
+++ uselang3/src/main/java/com/practise/useEnum/useEnums.java
@@ -7,12 +7,10 @@ public class useEnums {
     public static final Logger LOG=LoggerFactory.getLogger(useEnums.class);
 
     public static void main(String[] args) {
-
+//----------------------------------------------------------------------------------------------
         final ProjectClusterType hdfs = ProjectClusterType.valueOf("HDFS");//1、字符串转枚举
         System.out.println(hdfs.name());        //2、get名称
         System.out.println(hdfs.getValue());        //3、get value字段
-
-
 
     }
 }

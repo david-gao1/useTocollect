f02571007da5e5dab191a8942f929c3cb6328a74
v_liangggao <v_liangggao@email.com>
2020-09-18 20:02:26 +0800

    00007：自定义异常

---
+++ uselang3/src/main/java/com/practise/useException/UseException.java
@@ -0,0 +1,10 @@
+package com.practise.useException;
+
+public class UseException {
+    public static void main(String[] args) throws AgeException {
+        if (100>1){
+            throw new AgeException("年龄超过范围");
+            //会得到getMessage的信息
+        }
+    }
+}

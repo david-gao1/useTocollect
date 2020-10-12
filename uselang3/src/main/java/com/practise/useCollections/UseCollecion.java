8a6ac350824d9eebf5c075a9a53d64d3f9445073
v_liangggao <v_liangggao@email.com>
2020-10-12 15:36:06 +0800

    00012：集合类：流为空时不会执行

--- uselang3/src/main/java/com/practise/useCollections/UseCollecion.java
+++ uselang3/src/main/java/com/practise/useCollections/UseCollecion.java
@@ -8,9 +8,11 @@ public class UseCollecion {
         //object->list
         final List<String> st = Arrays.asList("st");
         //新建的Collection不为null而是[].
-        final Set set = new HashSet<String>();
+        final Set<String> set = new HashSet<String>();
         System.out.println(set); //[]
         System.out.println(set.isEmpty()); //true
+        set.stream().forEach(System.out::println);//流为空时不会执行。
+
         final List<String> arrayListStrings = new ArrayList<>();
         System.out.println(arrayListStrings); //[]
     }

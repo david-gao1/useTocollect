2d6d49b06f0d687b6c25e034958e257f864269bf
v_liangggao <v_liangggao@email.com>
2020-11-24 11:29:34 +0800

    00022：AES加密使用（两个组件间的调用使用，只有私钥没有公钥）

--- uselang3/src/main/java/com/practise/useStr/UserStr.java
+++ uselang3/src/main/java/com/practise/useStr/UserStr.java
@@ -1,8 +1,6 @@
 package com.practise.useStr;
 
 public class UserStr {
-    private static final String SPECIAL_WORD_REG_EX = "[ _`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]|\n|\r|\t";
-
 
     public static void main(String[] args) {
         //字符串是包含""的
